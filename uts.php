d67fc0836aad33def1a07bfbfd562b9a8f3cfd06
0wll <33440864+0wll@users.noreply.github.com>
2017-11-14 09:45:04 +0700

    Add files via upload

---
+++ uts.php
@@ -0,0 +1,39 @@
+<?php 
+	if(isset($_POST['='])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$operasi = $_POST['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bil1+$bil2;
+			break;
+			case 'kurang':
+				$hasil = $bil1-$bil2;
+			break;
+			case 'kali':
+				$hasil = $bil1*$bil2;
+			break;
+			case 'bagi':
+				$hasil = $bil1/$bil2;
+			break;			
+		}
+	}
+	?>
+	<div class="kalkulator">
+		<form method="post" action="uts.php">			
+			<input type="text" name="bil1" class="bil" autocomplete="off" placeholder="Bilangan Pertama">
+			<select class="opt" name="operasi">
+				<option value="tambah">+</option>
+				<option value="kurang">-</option>
+				<option value="kali">x</option>
+				<option value="bagi">/</option>
+			</select>
+			<input type="text" name="bil2" class="bil" autocomplete="off" placeholder="Bilangan Kedua">
+			
+			<input type="submit" name="=" value="=" class="tombol">											
+		
+		<?php if(isset($_POST['='])){ ?>
+			<input type="text" value="<?php echo $hasil; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?> </form>			
